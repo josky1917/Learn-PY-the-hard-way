6d61f4f6ba14894a90411d18ba59eb38b06a714c
zhujie <zhujie930@outlook.com>
2019-06-18 21:39:38 +0800

    Exercise1

---
+++ ex3.py
@@ -0,0 +1,16 @@
+my_name = 'Zhu Jie'
+my_age = 24
+my_height = 186
+my_weight = 140
+my_eye = 'Black'
+my_teeth = 'white'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eye} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+a1 = round(1.788)
+print(a1)
