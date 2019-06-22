e0feb1e5f32fbf76f361c98aabdce102c25f1ad6
josky1917 <zhujie930@outlook.com>
2019-06-22 15:41:37 +0800

    character encode & decode

---
+++ ex21.py
@@ -0,0 +1,25 @@
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+def substract(a, b):
+    print(f"SUBTRACTING {a}- {b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
+
+def divide(a, b):
+    print(f"DIVIDING {a} / {b}")
+    return a / b
+
+
+print("Let's do some math with just functions!")
+
+age = add(30, 5)
+height = substract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print(f"Age: {age}, Height: {height}, Wright: {weight}, IQ: {iq}")
